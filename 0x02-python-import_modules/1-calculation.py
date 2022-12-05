b4d75b35ffdcb31718f0386a5eb65da488071127
eniangnsa <adolph.eniang@gmail.com>
2022-12-05 06:31:11 -0800

    everything on modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,9 @@
-import calculator
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print(f"the sum of {:d} and {:d} is {:d}".format(a,b,add(a,b))
-print(f"the difference between {:d} and {:d} is {:d}".format(a,b,sub(a,b))
-print
+if (__name__ == '__main__'):
+    print(a, "+", b, "=", "{}".format(add(a, b)))
+    print(a, "-", b, "=", "{}".format(sub(a, b)))
+    print(a, "*", b, "=", "{}".format(mul(a, b)))
+    print(a, "/", b, "=", "{}".format(div(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,5 @@
+This project is about python modules. 
+Apparently, a module is just a normal python file that contains functions and statement. 
+Within a module, we can import other modules to make use  of functions in those modules.
+
+This important as it saves us  time to redefine functions everytime we might want to use them. 

9252039cfeb2f8fdf0a34892051791401489863f
eniangnsa <adolph.eniang@gmail.com>
2022-12-20 10:19:17 -0800

    added docs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This script builds a class with a private attribute
+Attributes:
+    Square (class): a class with a private attribute called 'size'
+"""
+
 class Sqaure:
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+A script that creates a class and validates its given attribute
+Attributes:
+    Square (class): with a valid attribute 'size'
+Todo:
+    * size must be an integer, otherwise raise a TypeError exception with
+    the message size must be an integer
+    * if size is less than 0, raise a ValueError exception with the message
+    size must be >= 0
+"""
+
+
 class Square:
     """A class with a 'size' attribute"""
     def __init__(self, size=0):

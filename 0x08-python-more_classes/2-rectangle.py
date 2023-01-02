5bfda879000b2b04286fc2b7a6be790382b238b4
eniangnsa <adolph.eniang@gmail.com>
2023-01-02 09:52:10 -0800

    rectangle class to calculate area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
-"""A rectangle class.
+
+"""
+A Rectangle class that extends module 1-rectangle.py
 TODO:
-    here i am just going to create a rectangle class.
-    For this class, i'll create some attributes and methods.
-    Then i'll customize my error messages.
-    That is  pretty much i'll be doing.
-    Calculate the area and perimeter.
+    * Create a public instance method: 'def area(self):'
+        that returns the rectangle area
+    * Create a public instance method: 'def perimeter(self):'
+        that returns the rectangle perimeter:
+        * if width or height is equal to 0, perimeter is equal to 0
+"""
 
-Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
 
-Private instance attribute: width:
-property def width(self): to retrieve it
-property setter def width(self, value): to set it:
-width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-if width is less than 0, raise a ValueError exception with the message width must be >= 0
-Private instance attribute: height:
-property def height(self): to retrieve it
-property setter def height(self, value): to set it:
-height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-if height is less than 0, raise a ValueError exception with the message height must be >= 0
-Instantiation with optional width and height: def __init__(self, width=0, height=0):
- """
 class Rectangle:
-    """ here i'll instantiate some attributes """
+    """
+    Attributes:
+        width (integer)
+        height (integer)
+    """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-
         """
-        args:
+        Args:
             width (integer)
             height (integer)
         """
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
         """
@@ -39,6 +33,7 @@ class Rectangle:
             width (integer)
         """
         return self.__width
+
     @property
     def height(self):
         """
@@ -46,41 +41,47 @@ class Rectangle:
             height (integer)
         """
         return self.__height
+
     @width.setter
     def width(self, value):
         """
         Args:
             value (integer)
-         """
+        """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @height.setter
     def height(self, value):
-        """"
+        """
         Args:
-            height (integer)
-
+            value (integer)
         """
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
-        """ calculate the area of a rectangle
-        Returns:
-            area (integer)
         """
-        return self.__height * self.width
+        Calculate the area of a rectangle
+        Returns:
+            area (integer):
+        """
+        return (self.width * self.height)
 
     def perimeter(self):
-        """ calculate the perimeter of a rectangle
+        """
+        Calculate the perimeter of a rectangle
         Returns:
             perimeter (integer)
-         """
-        return((2 * self.width) + (2 * self.height))
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * (self.width + self.height))
+
+            
